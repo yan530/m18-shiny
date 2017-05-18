13b150b79437e9272766488d17fd7feeae3d416e
Anni Yan <Anni Yan>
2017-05-17 17:15:53 -0700

    yea

--- exercise-5/server.R
+++ exercise-5/server.R
@@ -1,10 +1,14 @@
 #INFO 201 M18 Exercise 5 Server
 
 #Require the following: shiny, HSUAR, dyplr, and ggplot2
-
+library(shiny)
+library(dplyr)
+library(ggplot2)
+#install.packages('HSAUR')
+library(HSAUR)
 
 #Load the dataset 'womensrole' from the HSUAR package (same way you would load a built in dataset)
-
+data <- data.frame(womensrole)
 
 
 #Here you will define the data that is shown based on your inputs defined in the UI
@@ -22,17 +26,27 @@ shinyServer(function(input, output) {
     
     
     #Filter the dataset based on whether the input is Men, Women, or Both
-
+    if(input$sex == "Men") {
+      plot.data <- data %>% filter(sex == "Male")
+    } else if (input$sex == "Women") {
+      plot.data <- data %>% filter(sex == "Female")
+    } else {
+      plot.data <- data
+    }
   
     
     #Define a y axis value based on the input Agree or Disagree
-
+    if(input$thoughts == "Agree") {
+      thoughts = plot.data$agree
+    } else {
+      thoughts = plot.data$disagree
+    }
     
     #Using the values you just defined, construct a scatterplot using ggplot2
     #x will be defined by education, y by agree/disagree
     #Bonus: Factor the color field by sex 
     #(this will allow you to visually see the difference between Men and Women when Both are selected)
-
+    ggplot(plot.data, aes(x=education, y=thoughts, color=factor(sex))) + geom_point()
     
   })
   
--- exercise-5/ui.R
+++ exercise-5/ui.R
@@ -12,18 +12,24 @@ shinyUI(fluidPage(
   # Here you will add the interactivity to your app
   # Create a selectInput where the user can select either Men, Women, or Both. 
   # Create a selectInput where the user can select if they Agree or Disagree.
-
+  
   # Look at the Shiny documentation for selectInput and sliderInput syntax
   # inputIDs's have been provided
   sidebarLayout(
     sidebarPanel(
       #selectInput for Men, Women, or Both
       #inputID = sex
-      
+      selectInput(inputId = "sex",
+                  label = "Sex:",
+                  choices = c("Men", "Women", "Both"),
+                  selected = "Both"),
       
       #selectInput for Agree or Disagree
       #inputID = thoughts
-      
+      selectInput(inputId = "thoughts",
+                  label = "Who:",
+                  choices = c("Agree", "Disagree"),
+                  selected= "Agree")
       
     ),
     
